f1bfbd32f23afadd2271879a50bf5651663498d0
fujishig2 <fujishig@ualberta.ca>
2021-04-19 14:36:58 -0600

    scroll to top when changing nav

--- home/static/js/app.js
+++ home/static/js/app.js
@@ -81,6 +81,7 @@ burger.addEventListener('click', function() {
 var items = document.getElementsByClassName("navbar-item")
 for(var item of items) {
     item.addEventListener('click', function() {
+        window.scrollTo(0,0);
         burger.classList.remove('is-active');
         menu.classList.remove('is-active');
     });
