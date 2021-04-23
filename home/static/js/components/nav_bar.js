257e8fdbb88f90059103ae6c2e09c4fe31f8eeb4
fujishig2 <fujishig@ualberta.ca>
2021-04-23 16:37:07 -0600

    removed errors with keys in foreach calls and added background image capabilities

--- home/static/js/components/nav_bar.js
+++ home/static/js/components/nav_bar.js
@@ -18,7 +18,7 @@ class NavBarBrand extends React.Component {
         var items = []
         this.props.navData.forEach((item) => {
             if (item.brand) {
-                items.push(e(NavBarItem, {item: item, key: item.name}, {changeTab: this.props.children.changeTab}))
+                items.push(e(NavBarItem, {item: item, key: item.id}, {changeTab: this.props.children.changeTab}))
             }    
         })
         return (
@@ -39,7 +39,7 @@ class NavBarMenu extends React.Component {
         var items = []
         this.props.navData.forEach((item) => {
             if (!item.brand) {
-                items.push(e(NavBarItem, {item: item, key: item.name}, {changeTab: this.props.children.changeTab}))
+                items.push(e(NavBarItem, {item: item, key: item.id}, {changeTab: this.props.children.changeTab}))
             }    
         })
         return (
