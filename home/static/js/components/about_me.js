5532cc9d33156a45a4f0eec1336679671b00f54b
fujishig2 <fujishig@ualberta.ca>
2021-04-24 17:00:17 -0600

    added more padding for first post

--- home/static/js/components/about_me.js
+++ home/static/js/components/about_me.js
@@ -8,7 +8,7 @@ import HeroSection from "/static/js/components/hero.js";
 
 export default class AboutMe extends React.Component {
     render() {
-        var items = [e("br", {key: 0})]
+        var items = [e("br", {key: 0}), e("br", {key: 1})]
         var count = 0;
         posts.forEach((post) => {
             items.push(e(HeroSection, {post: post, key: post.id}, {theme:this.props.children.theme, post: post, count: count}));
