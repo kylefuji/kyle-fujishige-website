ceeb6d7d5d2139e058ac7324a339654493954942
kylefuji <kylefuji63@gmail.com>
2021-04-28 17:14:24 -0600

    fixed resume on navbar in dark mode

--- home/static/js/components/nav_bar.js
+++ home/static/js/components/nav_bar.js
@@ -74,7 +74,11 @@ class NavBarMenu extends React.Component {
             return (
                 e("div", {className: "navbar-menu", id:"navbarToggleMenu"}, 
                     e("div", {className: "navbar-start"},
-                        items
+                        items,
+                        e("a", {className: "navbar-item", target:"_blank", href: "https://docs.google.com/document/d/1jwxdyZrSfWOIfovwshYfbMGKN-G3-myvvVQb_x11_7g/edit?usp=sharing"}, 
+                            e("img", {src: "/static/icons/resume.svg", alt: "", width:"32px", height:"32px", className:"mr-2"}),
+                            "Resumé"
+                        )
                     ),
                     e("div", {className: "navbar-end"},
                         e("a", {className: "navbar-item", id:"toggle-theme", onClick: this.props.children.toggleTheme},
